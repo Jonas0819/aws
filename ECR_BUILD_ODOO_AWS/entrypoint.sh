87aaaae86b9c7f82cfbc6e366f80a8f418ba0151
Jonás Román Espaillat <48739623+Jonas0819@users.noreply.github.com>
2025-10-24 01:58:32 -0400

    Add files via upload

---
+++ ECR_BUILD_ODOO_AWS/entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+# set the postgres database host, port, user and password according to the environment
+# and pass them as arguments to the odoo process if not present in the config file
+: ${HOST:=${REDMINE_DB_POSTGRES:='db'}}
+: ${PORT:=${REDMINE_DB_PORT:=5432}}
+: ${USER:=${REDMINE_DB_USERNAME:=${POSTGRES_USER:='odoo'}}}
+: ${PASSWORD:=${REDMINE_DB_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+: ${FILTER:=${REDMINE_DB_DATABASE:='*'}}
+
+DB_ARGS=()
+function check_config() {
+    param="$1"
+    value="$2"
+    if ! grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then
+        DB_ARGS+=("--${param}")
+        DB_ARGS+=("${value}")
+   fi;
+}
+check_config "db_host" "$HOST"
+check_config "db_port" "$PORT"
+check_config "db_user" "$USER"
+check_config "db_password" "$PASSWORD"
+check_config "db-filter" "$FILTER"
+
+case "$1" in
+    -- | odoo)
+        shift
+        if [[ "$1" == "scaffold" ]] ; then
+            exec odoo "$@"
+        else
+            exec odoo "$@" "${DB_ARGS[@]}"
+        fi
+        ;;
+    -*)
+        exec odoo "$@" "${DB_ARGS[@]}"
+        ;;
+    *)
+        exec "$@"
+esac
+
+exit 1
